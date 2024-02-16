83eb783029b6129f372b7c1991d2cce2c73aaef6
karan <rajkaran208@gmail.com>
2024-02-16 12:29:43 +0530

    made commits

---
+++ src/main/java/com/devops/demojenkins/controller/AdditionController.java
@@ -0,0 +1,14 @@
+package com.devops.demojenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdditionController {
+
+    @GetMapping("/add")
+    public int addNumbers(@RequestParam int num1, @RequestParam int num2){
+        return num1 + num2;
+    }
+}
